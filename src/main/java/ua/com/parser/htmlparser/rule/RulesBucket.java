1b36409f07b659c6c1fb769145588920c0b1041f
Vadim Dyachenko <vadim.dyachenko@gmail.com>
2017-01-16 23:30:47 +0200

    Удалил лишние комментарии

--- src/main/java/ua/com/parser/htmlparser/rule/RulesBucket.java
+++ src/main/java/ua/com/parser/htmlparser/rule/RulesBucket.java
@@ -14,16 +14,12 @@ public class RulesBucket {
 
 
     public void createRule(String str) {
-        //vote>100
-        //view<=1000
-        //> < >= <= == !=
         Pattern pattern = Pattern.compile("[><=!]+");
         Matcher matcher = pattern.matcher(str);
         System.out.println(str);
         System.out.println(matcher.find() ?
                 "found '"+matcher.group()+"' starting at index "+matcher.start()+" and ending at index "+matcher.end()+"." :
                 "I found nothing!");
-// I found the text '192.168.0.3' starting at index 0 and ending at index 11.
     }
 
     public List<Rule> getRules() {
