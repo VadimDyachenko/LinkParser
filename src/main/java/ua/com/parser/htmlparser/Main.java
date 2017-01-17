8d5589a9e4513678e5c9099e17feaf88ebcbc130
heks <heksdev@gmail.com>
2017-01-17 21:53:05 +0200

    Merge branch 'join'
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/ua/com/parser/htmlparser/Main.java
+++ src/main/java/ua/com/parser/htmlparser/Main.java
@@ -2,32 +2,43 @@ package ua.com.parser.htmlparser;
 
 import ua.com.parser.htmlparser.fileworker.FileWorker;
 import ua.com.parser.htmlparser.fileworker.FileWorkerImpl;
+import ua.com.parser.htmlparser.parser.LinkParser;
+import ua.com.parser.htmlparser.rule.RulesBucket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vadim on 15.01.17.
  */
-//public class Main {
-//    public static void main(String[] args) {
-//
+public class Main {
+    public static void main(String[] args) {
+
 //        checkArgs(args);
-//        FileWorker fileWorker = new FileWorkerImpl();
-//
-//        try {
-//
+        FileWorker fileWorker = new FileWorkerImpl();
+
+        try {
+
 //            LinkCollector collector = new LinkCollector(fileWorker.read(args[0]));
 //            fileWorker.write(collector.getLinks());
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    private static void checkArgs(String[] args) { //TODO избавиться от static
-//        if (args.length != 2) {
-//            throw new IllegalArgumentException("Incorrect number input parameters\n" +
-//                    "You must input two path of file:\n" +
-//                    "The first file should contain conditions for comparison\n" +
-//                    "The second file for output data");
-//        }
-//    }
-//}
+            RulesBucket rulesBucket = new RulesBucket();
+            List<String> request = new ArrayList<>();
+            request.add("favorite==5");
+            rulesBucket.createRule(request);
+            LinkParser linkParser = new LinkParser("https://habrahabr.ru/hub/webdev/page882/", rulesBucket.getRules());
+            linkParser.call();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkArgs(String[] args) { //TODO избавиться от static
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Incorrect number input parameters\n" +
+                    "You must input two path of file:\n" +
+                    "The first file should contain conditions for comparison\n" +
+                    "The second file for output data");
+        }
+    }
+}
--- src/main/java/ua/com/parser/htmlparser/parser/Parser.java
+++ src/main/java/ua/com/parser/htmlparser/parser/Parser.java
@@ -6,22 +6,18 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-/**
- * Created by vadim on 16.01.17.
- */
-
 abstract class Parser {
 
     int getMaxPageNumber(String url) throws IOException {
+
         Document doc = Jsoup.connect(url).get();
         Elements links = doc.getElementById("nav-pages").getElementsByTag("li");
         String lastLink = links.last().child(0).attr("href");
 
-        return parseNumber(lastLink);
+        return lastLink.isEmpty() ? 0 : parseNumber(lastLink);
     }
 
     private int parseNumber(String lastLink) {
-
         return Integer.parseInt(lastLink.substring(lastLink.indexOf("page") + 4, lastLink.lastIndexOf('/')));
     }
 }
